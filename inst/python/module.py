9a975456db47ae5062f4bbc8bed8863b3d00188d
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-05-21 22:45:32 +0200

    Make leaner

--- inst/python/module.py
+++ inst/python/module.py
@@ -1,18 +1,5 @@
 import ipdb 
 
-# Add the QVEmod package to the system path. Needed to import corona_model as 
-# a module
-import sys
-import os
-filename = os.path.join(
-    os.path.dirname(__file__),
-    "../../dependencies/qvemod"
-)
-
-if not filename in sys.path:
-    sys.path.append(filename)
-
-# Import qvemod classes and the functions defined in our own python code
 from dependencies.qvemod.corona_model.model import Model
 
 from utility import select, dfs_to_object, df_to_object
