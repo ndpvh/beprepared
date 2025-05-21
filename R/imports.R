83a0626ce77d66f7eb482bf3d7e244db4f7b210c
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-05-21 22:45:07 +0200

    Import python functions onLoad

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,14 @@
 # Imports from other packages
 #' @importFrom magrittr %>%
 
-py <- reticulate::import_from_path("module", path = "./inst/python")
+reticulate::virtualenv_create("r-reticulate")
+.onLoad <- function(...) {
+    reticulate::py_require(system.file("python", "module", package = "beprepared"))
+    py <<-reticulate::import_from_path(
+        "module",
+        system.file("python", package = "beprepared"),
+        delay_load = TRUE
+    )
+}
 
 devtools::load_all(file.path("dependencies", "predped"))
