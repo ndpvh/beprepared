20fea2e13cef1c4bb2d3eb0a73a7a24bdec1ee7b
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-05-28 15:15:54 +0200

    Potential unreliability fix: Getting modules to load

--- R/imports.R
+++ R/imports.R
@@ -1,15 +1,20 @@
 # Imports from other packages
 #' @importFrom magrittr %>%
 
-reticulate::virtualenv_create("r-reticulate")
-.onLoad <- function(...) {
-    reticulate::py_require(system.file("python", "module", package = "beprepared"))
-    python_functions <<-reticulate::import_from_path(
-        "module",
-        system.file("python", package = "beprepared"),
-        delay_load = TRUE
-    )
-}
+# Preferred method, but unreliable
+# reticulate::virtualenv_create("r-reticulate")
+# .onLoad <- function(...) {
+#     reticulate::py_require(system.file("python", "module", package = "beprepared"))
+#     python_functions <<-reticulate::import_from_path(
+#         "module",
+#         system.file("python", package = "beprepared"),
+#         delay_load = TRUE
+#     )
+# }
+
+# Unpreferred method, but reliable
+reticulate::source_python(file.path("inst", "python", "module.py"))
+
 
 # devtools::install(file.path("dependencies", "predped"))
 devtools::load_all(file.path("dependencies", "predped"))
