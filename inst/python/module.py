b977fb355907e19eb046945541a375c693543509
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-05-28 16:30:24 +0200

    Fix import issue

--- inst/python/module.py
+++ inst/python/module.py
@@ -1,6 +1,6 @@
 import ipdb 
 
-from dependencies.qvemod.corona_model.model import Model
+from qvemod.corona_model.model import Model
 
 from utility import select, dfs_to_object, df_to_object
 from translate import translate_data, translate_env, translate_items, translate_row, translate_surf
