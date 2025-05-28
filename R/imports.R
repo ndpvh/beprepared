17ccf64c579cf23a5e0ffff189b46517d90774e9
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-05-28 16:15:04 +0200

    Fix import problems

--- R/imports.R
+++ R/imports.R
@@ -2,25 +2,15 @@
 #' @importFrom magrittr %>%
 
 # Preferred method, but unreliable
-# reticulate::virtualenv_create("r-reticulate")
-# .onLoad <- function(...) {
-#     reticulate::py_require(system.file("python", "module", package = "beprepared"))
-#     python_functions <<-reticulate::import_from_path(
-#         "module",
-#         system.file("python", package = "beprepared"),
-#         delay_load = TRUE
-#     )
-# }
+reticulate::virtualenv_create("r-reticulate")
+#' @export
+.onLoad <- function(...) {
+    reticulate::py_require(system.file("python", "module", package = "beprepared"))
+    python_functions <<-reticulate::import_from_path(
+        "module",
+        system.file("python", package = "beprepared"),
+        delay_load = TRUE
+    )
+}
 
-# Unpreferred method, but reliable
-#' @export 
-python_functions <- reticulate::import_from_path(
-    "module",
-    system.file("python", package = "beprepared"),
-    delay_load = TRUE
-)
-# reticulate::source_python(file.path("inst", "python", "module.py"))
-
-
-# devtools::install(file.path("dependencies", "predped"))
-devtools::load_all(file.path("dependencies", "predped"))
+devtools::load_all(system.file("predped", package = "beprepared"))
